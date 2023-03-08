c5a8238d235176b59f1fc756a1c9f80026152a26
matski <enzo.lagadec@etu.univ-savoie.fr>
2023-03-08 17:43:23 +0100

    Sah

---
+++ odoo/local-src/students/__manifest__.py
@@ -0,0 +1,11 @@
+{
+ "name": "Gestion des étudiants",
+ "version": "0.6",
+ "category": "Generic Modules/Others",
+ "description": """Test création module gestion des étudiants Odoo v14""",
+ "author": "Enzo",
+ "data": ["views/students_views.xml"],
+ "depends": ["base"],
+ "installable": True,
+ "auto_install": False,
+}
